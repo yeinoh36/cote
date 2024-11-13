0b687f676c39866bc10fd02ee47129eb09d84805
Yein Oh <147053671+yeinoh36@users.noreply.github.com>
2024-11-13 18:51:29 +0900

    [Silver III] Title: 수강신청, Time: 488 ms, Memory: 92280 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.py
+++ 백준/Silver/13414. 수강신청/수강신청.py
@@ -1,19 +1,13 @@
 import sys
-from collections import OrderedDict
 
 # 전체 입력받기
 data = sys.stdin.read().splitlines()
 K, L = map(int, data[0].split())
-click = OrderedDict()
+click = {}
+for i, student_id in enumerate(data[1:]):
+    click[student_id] = i
 
-# 두 번째 줄부터 마지막 줄까지 학번 처리
-for student_id in data[1:]:
-    if student_id in click:
-        del click[student_id]  # 기존 학번을 삭제하고
-    click[student_id] = None  # 최신 학번을 다시 추가하여 순서 유지
-
-# 최대 K명의 학번만 출력
-for i, student_id in enumerate(click.keys()):
-    if i >= K:
-        break
-    print(student_id)
+# 정렬 후 출력하기
+candidate = sorted(click.items(), key=lambda x: x[1])
+for i in range(min(K, len(candidate))):  # K와 click의 길이 중 작은 값 사용
+    print(candidate[i][0])
