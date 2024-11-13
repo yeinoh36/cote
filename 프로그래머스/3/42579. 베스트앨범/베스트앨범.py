764cda89e3aa6452fa98762bbf59dc6f87cf7a15
Yein Oh <147053671+yeinoh36@users.noreply.github.com>
2024-11-13 10:17:11 +0900

    [level 3] Title: 베스트앨범, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,28 +1,28 @@
 def solution(genres, plays):
-    # 장르 큰
-    # 재생 큰
-    # 고유번호 작은
     answer = []
-    play_gen = {}
-    genre_song_list = {}
-
-    for i, genre in enumerate(genres):
-        if genre in play_gen:
-            play_gen[genre] += plays[i]
-            genre_song_list[genre].append((plays[i], i))
+    gen_sum = {}
+    gen_plays = {}
+    
+    # 장르별 합 구하기
+    for i in range(len(plays)):
+        genre = genres[i]
+        play = plays[i]
+        if genre not in gen_sum.keys():
+            gen_sum[genre] = play
+            gen_plays[genre] = []
+            gen_plays[genre].append([play, i])
         else:
-            play_gen[genre] = plays[i]
-            genre_song_list[genre] = [(plays[i], i)]
-
-
-    sorted_play_gen=sorted(list(play_gen.items()), key=lambda x: x[1], reverse=True)
-    print(genre_song_list)
-
-    for genre in sorted_play_gen:
-        st = sorted(genre_song_list[genre[0]], key=lambda x:x[0], reverse = True)
-        if len(st) >= 2:
-            answer.append(st[0][1])
-            answer.append(st[1][1])
-        else:
-            answer.append(st[0][1])
+            gen_sum[genre] += play
+            gen_plays[genre].append([play, i])
+    
+    # 장르 순으로 두곡씩 수록하기
+    for gen, total in sorted(gen_sum.items(), key=lambda x: x[1], reverse=True):
+        tmp = sorted(gen_plays[gen], key=lambda x: x[0], reverse=True)
+        if len(tmp) == 1:
+            answer.append(tmp[0][1])
+            continue
+        
+        answer.append(tmp[0][1])
+        answer.append(tmp[1][1])
+    
     return answer
