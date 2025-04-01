c7309ca060a70674faeb3ac7cd76028867aab5d5
Yein Oh <147053671+yeinoh36@users.noreply.github.com>
2025-04-01 13:22:43 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 176 ms, Memory: 35508 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
@@ -0,0 +1,16 @@
+N = int(input())
+P = list(map(int, input().split()))
+
+dp = [0]*(N+1)
+for i in range(1, N+1):
+    dp[i] = P[i-1]
+    
+import heapq
+for i in range(2, N+1):
+    tmp = []
+    heapq.heappush(tmp, dp[i])
+    for j in range(1,i):
+        heapq.heappush(tmp, (dp[j] + dp[i-j]))
+    dp[i] = heapq.heappop(tmp)
+        
+print(dp[-1])
